05645c2edbe17ed7696aa81b7d0e8d333a2133ac
Swag-blips <coderblip@gmail.com>
2024-10-10 23:41:58 +0100

    feat:tailwind fix, initalized login page

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import mockup from "/assets/mockup.png";
+import logo from "/assets/logo.svg";
+import facebook from "/assets/Facebook.svg";
+import microsoft from "/assets/microsoft.png";
+import playstore from "/assets/playstore.png";
 
 const Login = () => {
-  return <div>Hello world from the login page</div>;
+  return (
+    <section className="flex items-center justify-center h-[100vh]">
+      <div className="flex gap-4 items-start">
+        <figure>
+          <img src={mockup} alt="login_mockup" />
+        </figure>
+
+        <div className="mt-8 flex flex-col w-[336px]">
+          <div className="flex items-center border flex-col border-[#DBDBDB]"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
